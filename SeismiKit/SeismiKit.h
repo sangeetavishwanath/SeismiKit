3a5cd38eea3adbf4035c38819a47feecfa20da2f
Sangeeta Vishwanath <sangeeta.vishwanath@outware.com.au>
2016-10-09 10:04:42 +1100

    Initial commit of Xcode project

---
+++ SeismiKit/SeismiKit.h
@@ -0,0 +1,19 @@
+//
+//  SeismiKit.h
+//  SeismiKit
+//
+//  Created by Sangeeta Vishwanath on 9/10/2016.
+//  Copyright © 2016 Sangeeta Vishwanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SeismiKit.
+FOUNDATION_EXPORT double SeismiKitVersionNumber;
+
+//! Project version string for SeismiKit.
+FOUNDATION_EXPORT const unsigned char SeismiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeismiKit/PublicHeader.h>
+
+
