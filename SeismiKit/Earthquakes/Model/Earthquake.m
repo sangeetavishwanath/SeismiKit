ff5f0f9122849e8e44a317988d1b1059d16b8eea
Sangeeta Vishwanath <sangeeta.vishwanath@gmail.com>
2016-10-09 12:51:48 +1100

    Adding Earthquake model

---
+++ SeismiKit/Earthquakes/Model/Earthquake.m
@@ -0,0 +1,13 @@
+//
+//  Earthquake.m
+//  SeismiKit
+//
+//  Created by Sangeeta Vishwanath on 9/10/2016.
+//  Copyright © 2016 Sangeeta Vishwanath. All rights reserved.
+//
+
+#import "Earthquake.h"
+
+@implementation Earthquake
+
+@end
